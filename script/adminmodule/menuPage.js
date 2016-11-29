dcbb642b56a392e09c255515cd2fc3f4da367a34
hottredpen <hottredpen@126.com>
2016-11-29 15:40:12 +0800

    采用nice表单验证

--- script/adminmodule/menuPage.js
+++ script/adminmodule/menuPage.js
@@ -1,13 +1,15 @@
 require.config({
     baseUrl: "/script/adminmodule",//用绝对位置
     shim: {
-        'validform'    : ['jquery']
+        'validator'    : ['../../script/lib/validator/dist/jquery.validator']
+        // 'validform'    : ['jquery'],
         // 'uploadify' : ['jquery'],
         // 'chat'      : ['jquery']
     },
     paths: {
         'jquery'          : "../../static/js/jquery",
-        'validform'       : "../../script/lib/validform/js/validform",
+        'validator'       : '../../script/lib/validator/dist/local/zh-CN',
+        //'validform'       : "../../script/lib/validform/js/validform",
         // 'uploadify'       : "../../static/cpk/plugins/uploadify/jquery.uploadify",
         // 'chat'            : "../../static/cpk/public/js/chat_all"
     },
@@ -17,7 +19,7 @@ require.config({
         }
     }
 });
-define(['jquery','dialog','common','commonTool'],function($,dialog){
+define(['jquery','dialog','common','commonTool','validator'],function($,dialog){
 
 
 
