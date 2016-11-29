4fa8a268142c44a4c0caaba1afc1df8db8b29572
hottredpen <hottredpen@126.com>
2016-11-29 15:47:54 +0800

    bootstrap

--- script/adminmodule/common.js
+++ script/adminmodule/common.js
@@ -23,12 +23,16 @@ define(['jquery','dialog'],function($,dialog){
         });
         return false;
     });
-
     $.validator.setTheme('bootstrap', {
         validClass: 'has-success',
         invalidClass: 'has-error',
         bindClassTo: '.form-group',
         formClass: 'n-default n-bootstrap',
         msgClass: 'n-right'
+    });
+    $('#j_artdialog_form').validator({
+        timely: 2,
+        stopOnError: true,
+        theme: 'yellow_right'
     });
 });
